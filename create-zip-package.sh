4ac8cdb67a369d90b00bc0322ff023588b85711f
Pongsakorn Ritrugsa <poundxi@protonmail.com>
2019-06-09 21:52:53 +0700

    Create zip directory before creating zip file

--- create-zip-package.sh
+++ create-zip-package.sh
@@ -6,6 +6,9 @@
 # read extension version from manifest.json
 version=$(grep '"version"' manifest.json | cut -d ':' -f 2 | sed 's/[", ]//g')
 
+# create zip directory
+mkdir zip > /dev/null 2>&1
+
 # create zip file
 zip -r zip/$version.zip \
 	3rdparty html_custom html_options icons js \
